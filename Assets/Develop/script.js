761f9914bbea7b1e8286e4ff40dacd9a37d0be05
Bardini <derekbardini@gmail.com>
2021-01-13 01:23:43 -0600

    city name and temp now appear on the page

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -1,17 +1,32 @@
 $( document ).ready(function() {
+
+    $("#searchcity").on("click enterKey", function (){
+        console.log("working");
+        let city =  $("#form1").val()
+        var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=70482e2b61f2d16121b8a6db0d8a674c"
     
-    var city = "Chicago" //will be user input later $("input").val()
-    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=70482e2b61f2d16121b8a6db0d8a674c"
-    
-    
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-        console.log(response.Runtime);
-      });
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+
+            let K = response.list[0].main.temp
+            let temp = (K - 273.15) * 1.80 + 32
+
+            $('#city').text(response.city.name); //need to add date
+            $("#weather-today").html("Temp: " + temp.toFixed(1) + "F°<br>Humidity:<br>Wind Speed:<br>UV Index:");
+            console.log(response.list[0].main.humidity);
+          
+           
+        
+        
+        
+        
+        
+        
+        });
+    })
 
 
 
