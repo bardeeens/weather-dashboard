8e65af98dbe26c439963c2f5744946c5b907349d
Bardini <derekbardini@gmail.com>
2021-01-12 21:42:24 -0600

    linked open weather, bootstrap, and google fonts

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -1,3 +1,21 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    
+    var city = "Chicago" //will be user input later $("input").val()
+    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=70482e2b61f2d16121b8a6db0d8a674c"
+    
+    
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        console.log(response.Runtime);
+      });
+
+
+
+
+
+
 });
